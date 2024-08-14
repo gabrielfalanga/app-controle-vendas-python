fee3ed03e1614e52cf7981a83bfebb495c51944a
gabrielfalanga <gabrielmartinsfalanga@gmail.com>
2024-08-14 09:25:11 -0300

    homepage estruturada | páginas criadas e linkadas

--- telas.py
+++ telas.py
@@ -7,3 +7,25 @@ class HomePage(Screen):
 
 class AjustesPage(Screen):
     pass
+
+
+class AdicionarVendasPage(Screen):
+    pass
+
+
+class ListarVendedoresPage(Screen):
+    pass
+
+
+class FotoPerfilPage(Screen):
+    pass
+
+
+class AdicionarVendedorPage(Screen):
+    pass
+
+
+class TodasVendasPage(Screen):
+    pass
+
+
