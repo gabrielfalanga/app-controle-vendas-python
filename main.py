4e8983feff6a4ba229145b92ab6fe05d0ffb30df
gabrielfalanga <gabrielmartinsfalanga@gmail.com>
2024-08-15 13:10:15 -0300

    bd no firebase criado e conectado para pegar o avatar de acordo com usuário

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.lang import Builder
 from telas import *
 from botoes import *
+import requests
 
 
 GUI = Builder.load_file('main.kv')
@@ -9,9 +10,23 @@ GUI = Builder.load_file('main.kv')
 
 class MainApp(App):
 
+    id_usuario = 2
+
     # retorna o GUI (interface visual definida em `main.kv`)
     def build(self):
         return GUI
+
+    def on_start(self):
+        # link do firebase com id do usuário atual
+        link = f'https://app-controle-vendas-812bb-default-rtdb.firebaseio.com/{self.id_usuario}.json'
+        # resposta da requisição como dicionário
+        requisicao = requests.get(link).json()
+        # campo 'avatar' do usuário atual no banco de dados
+        avatar = requisicao['avatar']
+        # pegando o id da foto de perfil do main.kv
+        foto_perfil = self.root.ids['foto_perfil']
+        # alterando a foto pelo id
+        foto_perfil.source = f"icones/fotos_perfil/{avatar}"
 
     def mudar_tela(self, id_tela_destino):
         # self.root é o 'main.kv'  |  ids é um dicionário com as telas seus ids
